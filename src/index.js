180dd3215879c2cc73a7527b82fb14416763db9a
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-07-29 13:23:56 +0200

    Uploading items for Portfolio

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './sass/main.scss';
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+    modalWindow: document.querySelector('[data-menu-window]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modalWindow.classList.toggle('animation-modal');
+  }
+})();
